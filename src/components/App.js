56a3faf4f4344412e6b227016d1b87e0cebe5820
Deepak Kumar Rout <deepakrout@hotmail.com>
2018-08-10 11:13:27 -0400

    Initial commit after project structure creation.

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React  from 'react';
+import PropTypes from 'prop-types';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <p>Header here..</p>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+App.prototype = {
+    children: PropTypes.object.isRequired
+};
+
+export default App;
